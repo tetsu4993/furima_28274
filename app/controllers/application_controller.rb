43ff3b2db4793add232bccbcf9b4bb73f81e5284
tetsu4993 <tetsu19901103@gmail.com>
2020-08-10 12:14:43 +0900

    BASIC認証の導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+  def index
+  end
+
+
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'shibuya' && password == '4993'
+    end
+  end
 end
