aa3f81d1f28206b5af48dc811b38f15a809fad17
tetsu4993 <tetsu19901103@gmail.com>
2020-08-12 12:10:34 +0900

    user-2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
 
 
@@ -9,4 +9,9 @@ class ApplicationController < ActionController::Base
       username == 'shibuya' && password == '4993'
     end
   end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :birthday, :family_name, :first_name, :family_name_kana, :first_name_kana])
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,13 @@ class User < ApplicationRecord
   has_many :items
   has_many :orders
 
- #with_options presence: true do
-   #validates :nickname, format: 
- #end
+  with_options presence: true do
+    #validates :password, format: {with: /\A[a-zA-Z0-9]+\z/, message: ""}
+    #validates :family_name, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: ""}
+    #validates :first_name, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: ""}
+    #validates :family_name_kana, format: {with: /\A[ァ-ヶー－]+\z/, message: ""}
+    #validates :first_name_kana, format: {with: /\A[ァ-ヶー－]+\z/, message: ""}
+  end
 
   
 end
