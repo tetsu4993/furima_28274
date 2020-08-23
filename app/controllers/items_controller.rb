3bd5b1dd20307354a794fe628ecdacfea0de9799
tetsu4993 <tetsu19901103@gmail.com>
2020-08-23 19:07:28 +0900

    Revert "商品詳細機能、商品削除機能の実装"
    This reverts commit ad9c94e8ba0c713c84c5c0b0ec1200ff28f3e563.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,23 +20,9 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
+    #@item.show
   end
 
-  def destroy
-    item = Item.find(params[:id])
-    item.destroy
-    redirect_to root_path
-  end
-
-  def edit
-    @item = Item.find(params[:id])
-  end
-
-  def update
-    item = Item.find(params[:id])
-    item.update(item_params)
-    redirect_to root_path
-  end
 
   private
 
